4c22a7f825260f64a410984a90dd707e1f908571
namelew <diogomaciel.cunha@gmail.com>
2022-09-21 14:32:31 -0300

    modify dfs and uploading graph.h

--- dfs_minor_path.c
+++ dfs_minor_path.c
@@ -1,45 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "graph.h"
 
-#define TAM 6
-
-typedef struct{
-    int value;
-    int parent;
-    int color;
-}Paths;
-
-void init(Paths *paths){
+void init(NodePath *paths){
     for(int i=0; i<TAM; i++){
         paths[i].value = 0;
         paths[i].parent = -1;
-        paths[i].color = 0;
+        paths[i].control = 0;
     }
 }
 
-void dfs(int g[TAM][TAM], Paths *path,int node){
+void dfs(int g[TAM][TAM], NodePath *path,int node){
     int i;
     printf("Explorando %d\n", node);
     for(i = 0; i < TAM; i++){
-        path[node].color = 1;
-        if(g[node][i] && !path[i].color){
+        path[node].control = 1;
+        if(g[node][i] && !path[i].control){
             path[i].parent = node;
             path[i].value = path[node].value + 1;
             dfs(g, path, i);
         }
-        path[node].color = 2;
+        path[node].control = 2;
     }
     printf("Explorados:\n");
     for(i = 0; i < TAM; i++){
-        if(path[i].color == 2){
+        if(path[i].control == 2)
             printf("%d(%d) ", i, path[i].value);
-        }
     }
-    printf("\nNão visitados:\n");
+    printf("\nNao visitados:\n");
     for(i = 0; i < TAM; i++){
-        if(!path[i].color){
+        if(!path[i].control)
             printf("%d(infinito) ", i);
-        }
     }
 }
 
@@ -54,15 +43,14 @@ int main(){
         {0,0,0,6,7,0}
     };
 
-    Paths paths[TAM];
+    NodePath paths[TAM];
 
     init(paths);
 
     dfs(graph, paths, 5);
     printf("           Caminho Final\n| Vertice | Distancia | Anterior |\n");
-    for(int i = 0; i < TAM; i++){
+    for(int i = 0; i < TAM; i++)
         printf("|    %i    |     %i     |     %i    |\n",i, paths[i].value, paths[i].parent);
-    }
 
     return 0;
 }
